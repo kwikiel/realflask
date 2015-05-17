b145fb4408d276816f0462afed29974dae00de95
Kacper <random8902@gmail.com>
2015-05-17 23:13:02 +0200

    Feature: Reading from db

---
+++ update.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+    c.execute("UPDATE population SET population = 2 WHERE city='WARSAW'")
+    c.execute("SELECT * FROM population")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print(r[0], r[1], r[2])
